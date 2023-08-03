a50875c3aacb960f7b500c75aa7e995b74ea1ccd
Enes Tanış <aenes.tanis@gmail.com>
2023-08-03 15:39:12 +0300

    About Page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,32 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+import React from "react";
+import styled from "styled-components";
+import { PageHero } from "../components";
+import aboutImg from "../assets/hero-bcg.jpeg";
 
 const AboutPage = () => {
-  return <h4>about page</h4>
-}
+  return (
+    <main>
+      <PageHero title='about' />
+      <Wrapper className='page section section-center'>
+        <img src={aboutImg} alt='nice desk' />
+        <article>
+          <div className='title'>
+            <h2>out story</h2>
+            <div className='underline'></div>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error,
+              in. Fugiat adipisci incidunt, aspernatur nisi repellendus ea ipsa
+              amet quia aliquid sapiente ratione delectus unde. Hic, ex deleniti
+              rem deserunt facere suscipit ad asperiores sint molestiae
+              accusantium magnam. Quibusdam obcaecati pariatur tempora dolores?
+              Officia, quae praesentium. Quod voluptatem sit omnis.
+            </p>
+          </div>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
 
 const Wrapper = styled.section`
   display: grid;
@@ -33,5 +54,5 @@ const Wrapper = styled.section`
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
-`
-export default AboutPage
+`;
+export default AboutPage;
